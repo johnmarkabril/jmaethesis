7b4d45df28f6bcb84514fee1d831ebfa3bc778a6
Farrah Mae Gregorio <frrhmgrgrio@gmail.com>
2016-12-10 16:23:36 +0800

    12/10/16

--- application/views/user/events.php
+++ application/views/user/events.php
@@ -1,5 +1,7 @@
-<?php
+<div class="landing-page">
+	<?php
 
-	$this->load->view('user/landingpage/carousel');
-
-?>
+		$this->load->view('user/landingpage/carousel');
+		$this->load->view('user/events_content');
+	?>	
+</div>
